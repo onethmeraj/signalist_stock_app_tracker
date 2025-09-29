6c987c198cbf266dcdc48bc4153585b66884c6d9
Oneth Meraj <onethmeraj56@gmail.com>
2025-09-29 11:22:35 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <Button>Click Me</Button>
+        </div>
+    )
+}
+export default Page
